800a8ed18167c3d40e8075be0b0342867dfaddc1
criscaldeiraa <criscaldeirat@gmail.com>
2024-06-17 17:45:52 +0100

    service-worker

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import { Routes, Route } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 
-import { useEffect } from "react";
-
+import { Suspense, useEffect } from "react";
+import { GlobalStyle } from './global.styles';
 import Navigation from './routes/navigation/navigation.component';
 import Home from './routes/home/home.component';
 import Authentication from './routes/authenticator/authentication.component';
 import Shop from './routes/shop/shop.component';
 import Checkout from './routes/checkout/checkout.component';
 import { checkUserSession } from './store/user/user.action';
+import Spinner from './components/spinner/spinner.component';
 
 const App = () => {
   const dispatch = useDispatch();
@@ -19,14 +20,17 @@ const App = () => {
   }, [])
 
   return (
-    <Routes>
-      <Route path='/' element={<Navigation />}>
-        <Route index={true} element={<Home/>} />
-        <Route path='shop/*' element={<Shop/>} />
-        <Route path='auth' element={<Authentication/>} />
-        <Route path='checkout' element={<Checkout />} />
-      </Route>
-    </Routes>
+    <Suspense fallback={<Spinner/>}>
+      <GlobalStyle />
+      <Routes>
+        <Route path='/' element={<Navigation />}>
+          <Route index={true} element={<Home/>} />
+          <Route path='shop/*' element={<Shop/>} />
+          <Route path='auth' element={<Authentication/>} />
+          <Route path='checkout' element={<Checkout />} />
+        </Route>
+      </Routes>
+    </Suspense>
   )
 }
 
